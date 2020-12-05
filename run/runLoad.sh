eaf2157aefb261c90e809ef26ed58cfa7eabe1e9
jos <julian.osmer@outlook.de>
2020-12-06 00:11:34 +0100

    update

--- run/runLoad.sh
+++ run/runLoad.sh
@@ -2,17 +2,17 @@
 
 # Run the drop.sql batch file to drop existing tables.
 # Inside the drop.sql, you should check whether the table exists. Drop them ONLY if they exist.
-mysql < drop.sql
+mysql <drop.sql
 
 # Run the create.sql batch file to create the database (if it does not exist) and the tables.
-mysql < create.sql
+mysql <create.sql
 
 # Compile and run the convertor
 javac MyDOM.java
 java MyDOM
 
 # Run the load.sql batch file to load the data
-mysql ad < load.sql
+mysql ad <load.sql
 
 rm bids.csv
 rm categories.csv
